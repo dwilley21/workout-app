6ac85726d031f0923877bda2c33bf07219b20e59
dwilley21 <daltonwilley@gmail.com>
2019-06-06 15:49:00 -0500

    toggle working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Workouts from './components/Workouts';
 import User from './components/User';
 import Add from './components/Add';
+import Toggle from './components/Toggle'; 
 
 function App() {
   return (
@@ -12,6 +13,16 @@ function App() {
       <User />
       <Add />
       <Workouts />
+      <Toggle  
+        render={({on, toggle}) =>(
+        <div>
+          {/* a b workout connections go here */}
+          {on && <h1>Show me</h1>}
+        
+        <button onClick={toggle}> A/B </button>
+        </div>
+      )}
+    />
     </div>
   );
 }
